11394e12e4a7135d868c186b9a26a7d42852b5f8
Hugo Zoells <hugozoells@users-MacBook-Pro-2.local>
2020-06-10 14:40:22 -0500

    add a working accordion

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -3,29 +3,40 @@ import {Link} from 'react-router-dom';
 import './styles/MenuBar.scss';
 import hamburger from './images/hamburger.svg';
 class MenuBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            menuOpen:false
+        }
+        this.toggleMenu=this.toggleMenu.bind(this);
+    }
+    toggleMenu(){
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        });
+    }
 
-  render(){
-    return (
-        <div className="menubar_container">
-            <span className="menubar_icon">
-                Hugo <img className="menubar_icon_photo" src={hamburger} alt=""/> Zoells
-            </span>
-            <Link className="menubar_item" to="/contact">
-                Hugo Zoells
-            </Link>
-            <Link className="menubar_item menubar_item_page" to="/experience">
-                Experience
-            </Link>
-            <Link className="menubar_item menubar_item_page" to="/work">
-                Work Samples
-            </Link>
-            <Link className="menubar_item menubar_item_page" to="/personal">
-                Hobbies
-            </Link>
-
-        </div>
-    );
-  }
+    render(){
+        return (
+            <div className={`menubar_container ${this.state.menuOpen?'accordion':''}`}>
+                <span className="menubar_icon" onClick={this.toggleMenu}>
+                    Hugo <img className="menubar_icon_photo" src={hamburger} alt=""/> Zoells
+                </span>
+                <Link className={`menubar_item menubar_item_page ${this.state.menuOpen ? "show":""}`} to="/contact" onClick={this.toggleMenu}>
+                    Hugo Zoells
+                </Link>
+                <Link className={`menubar_item menubar_item_page ${this.state.menuOpen ? "show":""}`} to="/experience" onClick={this.toggleMenu}>
+                    Experience
+                </Link>
+                <Link className={`menubar_item menubar_item_page ${this.state.menuOpen ? "show":""}`} to="/work" onClick={this.toggleMenu}>
+                    Work Samples
+                </Link>
+                <Link className={`menubar_item menubar_item_page ${this.state.menuOpen ? "show":""}`} to="/personal" onClick={this.toggleMenu}>
+                    Hobbies
+                </Link>
+            </div>
+        );
+    }
 }
 
 export default MenuBar;
